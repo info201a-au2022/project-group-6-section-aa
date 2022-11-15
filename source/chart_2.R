f083fc0f1f513c4e9ba778c4de5ca2a538ad0b33
eunjiii-2029344 <eunjiii@uw.edu>
2022-11-14 23:35:06 -0800

    progress

--- source/chart_2.R
+++ source/chart_2.R
@@ -1,9 +1,14 @@
 # scatterplot with gdp and education attainment
 library(dplyr)
 library(ggplot2)
+library(tidyverse)
 
 education <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-6-section-aa/main/data/Educational.csv")
 gdp <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-6-section-aa/main/data/GDP(current%20%20).csv")
+
+education <- replace(education, education == "-", 0) %>% 
+  mutate(years_2016_2021 = as.numeric(X2016) + as.numeric(X2017) + as.numeric(X2018) + as.numeric(X2019) +as.numeric(X2021) , na.rm = T) %>% 
+  rename(Country.Name = Time.period)
 
 new_gdp_values <- gdp %>% 
   select(Country.Name, X2020)
@@ -14,9 +19,9 @@ new_education <- education %>%
 gdp_education <- left_join(new_education, new_gdp_values, by = "Country.Name") %>% 
   rename(education_attainment = years_2016_2021, gdp = X2020)
 
-gdp_education_long <- gdp_education %>% pivot_longer(!Country.Name, names_to = "Type",values_to = "Count" )
+gdp_education_long <- gdp_education %>% pivot_longer(!Country.Name, names_to = "Type", values_to = "Count" )
 barchart_gdp_education <- ggplot(gdp_education_long, aes(x = Country.Name, y = Count, fill = Type)) +
-  geom_col(position = position_dodge(0.7))
+  geom_col(position = position_dodge(0.7)) +
   labs(title = "GDP versus Fertility rate")
 
 plot(barchart_gdp_education)
