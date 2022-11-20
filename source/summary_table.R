063418cc4302c1a6c0cd8ee22987bee4c8eeaa1d
eunjiii-2029344 <eunjiii@uw.edu>
2022-11-19 23:46:08 -0800

    why

--- source/summary_table.R
+++ source/summary_table.R
@@ -7,7 +7,8 @@ mortality <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project
 reproductive <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-6-section-aa/main/data/reproductiveAgeWomen.csv")
 
 new_gdp <- gdp %>% 
-  select(Country.Name, X2020)
+  select(Country.Name, X2020) %>% 
+  rename(years = X2020)
 new_gdp <- new_gdp %>%
   mutate(indicator = NA)
 vec_1 <- "gdp"
@@ -16,9 +17,9 @@ new_gdp <- new_gdp %>%
 
 new_education <- education %>% 
   replace(education == "-", 0) %>% 
-  mutate(years_2016_2021 = as.numeric(X2016) + as.numeric(X2017) + as.numeric(X2018) + as.numeric(X2019) +as.numeric(X2021) , na.rm = T) %>% 
+  mutate(years = as.numeric(X2016) + as.numeric(X2017) + as.numeric(X2018) + as.numeric(X2019) +as.numeric(X2021) , na.rm = T) %>% 
   rename(Country.Name = Time.period) %>% 
-  select(Country.Name, years_2016_2021)
+  select(Country.Name, years)
 new_education <- new_education %>% 
   mutate(indicator = NA)
 vec_2 <- "education"
@@ -26,7 +27,8 @@ new_education <- new_education %>%
   mutate(indicator = vec_2)
 
 new_fertility <- fertility %>% 
-  select(Country.Name, X2020)
+  select(Country.Name, X2020) %>% 
+  rename(years = X2020)
 new_fertility <- new_fertility %>% 
   mutate(indicator = NA)
 vec_3 <- "fertility"
@@ -34,7 +36,8 @@ new_fertility <- new_fertility %>%
   mutate(indicator = vec_3)  
 
 new_mortality <- mortality %>% 
-  select(Country.Name, X2020)
+  select(Country.Name, X2020) %>% 
+  rename(years = X2020)
 new_mortality <- new_mortality %>% 
   mutate(indicator = NA)
 vec_4 <- "mortality"
@@ -42,9 +45,9 @@ new_mortality <- new_mortality %>%
   mutate(indicator = vec_4)   
 
 new_reproductive <- reproductive %>% 
-  rename(Country.Name = Location)
+  rename(Country.Name = Location, years = First.Tooltip)
 new_reproductive <- new_reproductive %>% 
-  select(Country.Name, First.Tooltip)
+  select(Country.Name, years)
 new_reproductive <- new_reproductive %>% 
   mutate(indicator = NA)
 vec_5 <- "reproductive"
@@ -54,4 +57,5 @@ new_reproductive <- new_reproductive %>%
 merged <- bind_rows(new_gdp, new_education, new_fertility, new_mortality, new_reproductive)
 
 summary <- merged %>% 
-  group_by(Country.Name)
+  group_by(Country.Name) %>% 
+  arrange(Country.Name)
