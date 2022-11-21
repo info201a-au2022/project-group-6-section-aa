a2b9710c78ef50fc70c89cf37b34ab2d2b3f889f
eunjiii-2029344 <eunjiii@uw.edu>
2022-11-21 12:22:30 -0800

    progress

--- source/summary_table.R
+++ source/summary_table.R
@@ -11,9 +11,25 @@ new_gdp <- gdp %>%
   rename(years = X2020)
 new_gdp <- new_gdp %>%
   mutate(indicator = NA)
+# take it out of the vector
 vec_1 <- "gdp"
 new_gdp <- new_gdp %>%
   mutate(indicator = vec_1)
+
+# Testing
+hi <- gdp %>% 
+  select(Country.Name, X2020) %>% 
+  filter(X2020 == max(X2020), na.rm = TRUE)
+
+testtest <- gdp[, c('Country.Name', 'X2020')]
+max(testtest$X2020, na.rm = TRUE)
+
+
+
+?type
+type(testtest$X2020)
+
+View(gdp)
 
 new_education <- education %>% 
   replace(education == "-", 0) %>% 
@@ -26,6 +42,13 @@ vec_2 <- "education"
 new_education <- new_education %>%
   mutate(indicator = vec_2)
 
+# Testing
+hello <- education %>% replace(education == "-", 0) %>% 
+  mutate(years = as.numeric(X2016) + as.numeric(X2017) + as.numeric(X2018) + as.numeric(X2019) +as.numeric(X2021) , na.rm = T) %>% 
+  rename(Country.Name = Time.period) %>% 
+  select(Country.Name, years) %>% 
+  summarize(years = max(years), na.rm = TRUE)
+
 new_fertility <- fertility %>% 
   select(Country.Name, X2020) %>% 
   rename(years = X2020)
@@ -35,6 +58,11 @@ vec_3 <- "fertility"
 new_fertility <- new_fertility %>%
   mutate(indicator = vec_3)  
 
+# Testing
+bye <- fertility %>% 
+  select(Country.Name, X2020) %>% 
+  filter(X2020 == max(X2020), na.rm = TRUE)
+
 new_mortality <- mortality %>% 
   select(Country.Name, X2020) %>% 
   rename(years = X2020)
@@ -43,6 +71,11 @@ new_mortality <- new_mortality %>%
 vec_4 <- "mortality"
 new_mortality <- new_mortality %>%
   mutate(indicator = vec_4)   
+
+# Testing
+bye_2 <- mortality %>% 
+  select(Country.Name, X2020) %>% 
+  summarize(X2020 = max(X2020), na.rm = TRUE)
 
 new_reproductive <- reproductive %>% 
   rename(Country.Name = Location, years = First.Tooltip)
@@ -57,5 +90,8 @@ new_reproductive <- new_reproductive %>%
 merged <- bind_rows(new_gdp, new_education, new_fertility, new_mortality, new_reproductive)
 
 summary <- merged %>% 
-  group_by(Country.Name) %>% 
+  group_by(indicator) %>% 
+  summarize(years = max(years, na.rm = TRUE)) %>% 
+  filter(years == max(years)) %>% 
+  pull(Country.Name)
   arrange(Country.Name)
