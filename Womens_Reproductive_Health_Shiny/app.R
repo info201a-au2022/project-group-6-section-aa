2d8703641ad5177e55cb84c07881bd272914af5a
krendu <krendu@uw.edu>
2022-12-07 17:05:42 -0800

    final change

--- Womens_Reproductive_Health_Shiny/app.R
+++ Womens_Reproductive_Health_Shiny/app.R
@@ -21,8 +21,6 @@ source("server.R")
 # Run the application 
 shinyApp(ui = ui, server = server)
 
-rsconnect::setAccountInfo(name='womenshealth', 
-                          token='766E313306714232614E9F52E3B4C389', 
-                          secret='h7CtmQ4Fd0tGDfqjJlhWfU9ZlTiUGnAfcrK276YE')
+
 
 
